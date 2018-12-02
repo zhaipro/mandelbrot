f7ce518aaee32a29fdef8280535965ff04f0a294
yifuda <xin_soft@foxmail.com>
2018-12-02 20:55:56 +0800

    Change name of function.

--- mandelbrot.py
+++ mandelbrot.py
@@ -3,7 +3,7 @@ import numpy as np
 
 # counts the number of iterations until the function diverges or
 # returns the iteration threshold that we check until
-def count_iterations_until_divergent(c, threshold):
+def mandelbrot(c, threshold):
     z = complex(0, 0)
     for iteration in range(threshold):
         z = (z * z) + c
@@ -15,8 +15,8 @@ def count_iterations_until_divergent(c, threshold):
 # takes the iteration limit before declaring function as convergent and
 # takes the density of the atlas
 # create atlas, plot mandelbrot set, display set
-def mandelbrot(xmin=-2.25, xmax=0.75, ymin=-1.5, ymax=1.5,
-               threshold=120, density=1000):
+def mandelbrot_set(xmin=-2.25, xmax=0.75, ymin=-1.5, ymax=1.5,
+                   threshold=120, density=1000):
     # location and size of the atlas rectangle
     real_axis = np.linspace(xmin, xmax, density)
     imaginary_axis = np.linspace(ymin, ymax, density)
@@ -27,14 +27,14 @@ def mandelbrot(xmin=-2.25, xmax=0.75, ymin=-1.5, ymax=1.5,
     for ix, cx in enumerate(real_axis):
         for iy, cy in enumerate(imaginary_axis):
             c = complex(cx, cy)
-            atlas[iy, ix] = count_iterations_until_divergent(c, threshold)
+            atlas[iy, ix] = mandelbrot(c, threshold)
     return atlas
 
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
     # time to party!!
-    atlas = mandelbrot(-0.22, -0.219, -0.70, -0.699)
+    atlas = mandelbrot_set(-0.22, -0.219, -0.70, -0.699)
     # plot and display mandelbrot set
     plt.imshow(atlas, interpolation="nearest")
     plt.show()
