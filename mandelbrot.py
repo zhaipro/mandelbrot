589c28cbf9552cb0f8fda3204ae7f7a53f3012d2
yifuda <xin_soft@foxmail.com>
2018-12-02 21:58:22 +0800

    Beautify.

--- mandelbrot.py
+++ mandelbrot.py
@@ -7,21 +7,22 @@ def mandelbrot(c, threshold):
     z = complex(0, 0)
     for iteration in range(threshold):
         z = (z * z) + c
-        if abs(z) > 4:
-            break
-    return iteration
+        if abs(z) > 2:
+            return iteration
+    return 0
 
 
 # takes the iteration limit before declaring function as convergent and
 # takes the density of the atlas
 # create atlas, plot mandelbrot set, display set
 def mandelbrot_set(xmin=-2.25, xmax=0.75, ymin=-1.5, ymax=1.5,
-                   threshold=120, density=1000):
+                   threshold=120, xn=1000):
+    yn = int((ymax - ymin) / (xmax - xmin) * xn)
     # location and size of the atlas rectangle
-    real_axis = np.linspace(xmin, xmax, density)
-    imaginary_axis = np.linspace(ymin, ymax, density)
+    real_axis = np.linspace(xmin, xmax, xn)
+    imaginary_axis = np.linspace(ymin, ymax, yn)
     # 2-D array to represent mandelbrot atlas
-    atlas = np.empty((density, density))
+    atlas = np.empty((yn, xn))
 
     # color each point in the atlas depending on the iteration count
     for ix, cx in enumerate(real_axis):
@@ -35,6 +36,7 @@ if __name__ == '__main__':
     import time
     import matplotlib.pyplot as plt
     # time to party!!
-    atlas = mandelbrot_set(-0.22, -0.219, -0.70, -0.699)
+    atlas = mandelbrot_set(-0.748768, -0.748718, 0.0650619375, 0.0650900625,
+                           threshold=2048, xn=1920)
     # save mandelbrot set
-    plt.imsave(f'{time.time():.0f}.jpg', atlas)
+    plt.imsave(f'{time.time():.0f}.jpg', atlas, cmap='gnuplot2')
