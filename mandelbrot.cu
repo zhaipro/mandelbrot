27d43bfc45f19621a838c8cdcaf3ca313c0cc74c
lixin <lixin@moviebook.cn>
2020-11-26 15:51:31 +0800

    nvcc mandelbrot.cu

--- mandelbrot.cu
+++ mandelbrot.cu
@@ -1,4 +1,7 @@
+#include <stdio.h>
+
 #include <cuComplex.h>
+
 
 #define CUDA_KERNEL_LOOP_x(i,n) \
     for(int i = blockIdx.x * blockDim.x + threadIdx.x; \
@@ -35,4 +38,35 @@ __global__ void mandelbrot_set(double xmin, double xmax, double ymin, double yma
             atlas[j * xn + i] = mandelbrot(c, threshold);
         }
     }
+}
+
+int main()
+{
+    int *host_atlas = nullptr;
+    int *device_atlas = nullptr;
+
+    host_atlas = (int*)malloc(1920 * 1080 * sizeof(int));
+    cudaMalloc((void**) &device_atlas, 1920 * 1080 * sizeof(int));
+    dim3 d(16, 16, 1);
+    mandelbrot_set<<<d, d>>>(-0.748768, -0.748718, 0.0650619375, 0.0650900625, 1920, 1080, 2048, device_atlas);
+    cudaMemcpy(host_atlas, device_atlas, 1920 * 1080 * sizeof(int), cudaMemcpyDeviceToHost);
+
+    FILE *fp = fopen("MathPic.ppm","wb");
+    int max = 0;
+    fprintf(fp, "P6\n%d %d\n255\n", 1920, 1080);
+    for(int i = 0; i < 1920 * 1080; i++)
+    {
+        if(host_atlas[i] > max)
+            max = host_atlas[i];
+    }
+    for(int i = 0; i < 1920 * 1080; i++)
+    {
+        char c = host_atlas[i] * 255 / max;
+        fwrite(&c, 1, 1, fp);
+        fwrite(&c, 1, 1, fp);
+        fwrite(&c, 1, 1, fp);
+    }
+    fclose(fp);
+
+    return 0;
 }
